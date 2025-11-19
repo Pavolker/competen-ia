436056cb0d191971f293fdfb10fcf8236386823b
Pavolker <pavolker@github.com>
2025-11-19 09:14:58 -0300

    fix: configure build output directory for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig(({ mode }) => {
   return {
     root: 'src',
     publicDir: '../public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
     server: {
       port: 3000,
       host: '0.0.0.0',
